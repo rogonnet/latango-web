81099b4355cd844d29309b8d3ae47f76e2099b05
jasvin liyun <jasvinliyun@gmail.com>
2017-03-28 14:15:58 +0800

    add npm server express

--- public/img/js/style.js
+++
@@ -1,6 +0,0 @@
-$(function() {
-    var k = $("#header").load("header.html");
-    $("#banner").load("banner.html");
-    $("#footer").load("footer.html");
-    console.log(k);
-});
---
+++ public/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+
+// Define the port to run on
+app.set('port', 8000);
+
+app.use("/css", express.static("css"));
+app.use("/img", express.static("img"));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+// Listen for requests
+var server = app.listen(app.get('port'), function() {
+    var port = server.address().port;
+    console.log('Running on port ' + port);
+});
